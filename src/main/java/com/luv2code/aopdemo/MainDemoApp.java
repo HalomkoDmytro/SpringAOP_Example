67eae2847f6bcbc982ee62899b5db6f76ce42787
Dmytro Galomko <galomko.dmytro@gmail.com>
2018-12-11 17:11:30 +0200

    init example

---
+++ src/main/java/com/luv2code/aopdemo/MainDemoApp.java
@@ -0,0 +1,23 @@
+package com.luv2code.aopdemo;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainDemoApp {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+        MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+
+        theAccountDAO.addAccount();
+
+        System.out.println("Call again\n");
+
+        theMembershipDAO.addAccount();
+
+        context.close();
+    }
+}
